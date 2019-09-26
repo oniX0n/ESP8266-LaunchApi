5dcf7af8274900fd053cef2801d7dbb2888b8380
Theo Müller <39184576+oniX0n@users.noreply.github.com>
2019-09-26 18:08:06 +0200

    Add ESP8266-HttpsGet.ino

---
+++ ESP8266-HttpsGet/ESP8266-HttpsGet.ino
@@ -0,0 +1,72 @@
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClientSecureBearSSL.h>
+
+ESP8266WiFiMulti WiFiMulti;
+
+void setup() {
+
+  Serial.begin(115200);
+
+  Serial.println();
+  Serial.println();
+  Serial.println();
+
+  for (uint8_t t = 4; t > 0; t--) {
+    Serial.printf("[SETUP] WAIT %d...\n", t);
+    Serial.flush();
+    delay(1000);
+  }
+
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP("FRITZ!Box 7490", "91771257515399014601");
+}
+
+void loop() {
+ Serial.println(getHttpsString("https://api.shackspace.de/v1/space"));
+ delay(60000);
+}
+
+String getHttpsString(String url) {
+  String payload = "";
+  if ((WiFiMulti.run() == WL_CONNECTED)) {
+    Serial.println("Wifi Ok!");
+    
+    std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
+
+    //client->setFingerprint(fingerprint);
+    client->setInsecure();
+
+    HTTPClient https;
+
+    Serial.print("[HTTPS] begin...\n");
+    if (https.begin(*client, url)) {  // HTTPS
+
+      Serial.print("[HTTPS] GET...\n");
+      // start connection and send HTTP header
+      int httpCode = https.GET();
+
+      // httpCode will be negative on error
+      if (httpCode > 0) {
+        // HTTP header has been send and Server response header has been handled
+        Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
+
+        // file found at server
+        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+          payload = https.getString();
+        }
+      } else {
+        Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+      }
+
+      https.end();
+    } else {
+      Serial.printf("[HTTPS] Unable to connect\n");
+    }
+  }
+  else Serial.println("Wifi ERROR");
+  return payload;
+}
